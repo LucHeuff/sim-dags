362d093bf63b9cd6f3c64edfe5f040f38af4c365
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2026-03-24 11:22:49 +0100

    Added new functions to init

--- src/sim_dags/__init__.py
+++ src/sim_dags/__init__.py
@@ -1,2 +1,9 @@
 # ruff: noqa: F401
-from sim_dags.probability import p, p_array
+from sim_dags.probability import (
+    log_p_grid,
+    log_p_grid_array,
+    p,
+    p_array,
+    p_grid,
+    p_grid_array,
+)
