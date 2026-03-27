acfe10b04d8f0a0a48bd76e752237cd92d0ba66d
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2026-03-27 08:58:23 +0100

    Added sim iterations and removed direct import for log functions

--- src/sim_dags/__init__.py
+++ src/sim_dags/__init__.py
@@ -1,7 +1,6 @@
 # ruff: noqa: F401
+from sim_dags.iterate_sims import iterate_samples, plot_samples
 from sim_dags.probability import (
-    log_p_grid,
-    log_p_grid_array,
     p,
     p_array,
     p_grid,
